8bace4170c4f50c1dbd9aca6218c76e315a88262
Taras Mankovski <tarasm@gmail.com>
2016-07-05 20:59:29 -0400

    Added test for execution

--- tests/integration/components/shhh-helper-test.js
+++ tests/integration/components/shhh-helper-test.js
@@ -19,7 +19,7 @@ describeComponent(
       expect(this.$()).to.have.length(1);
     });
 
-    it(`prevents output from being rendered`, function(){
+    it('prevents output from being rendered', function(){
 
       let echoCalled = 0;
       let lastMessage;
@@ -42,6 +42,39 @@ describeComponent(
       expect(lastMessage).to.equal('BAR');
       expect(this.$().text()).to.equal('');      
 
+    });
+
+    it('allows callback to be executed with compute', function(){
+
+      let called = 0;
+      let result;
+
+      this.set('firstStep', function(value){
+        called++;
+        result = `first step: ${value}`;
+        return value;
+      });
+
+      this.set('secondStep', function(value){
+        called++;
+        result = `second step: ${value}`;
+        return value;
+      });
+
+      this.set('takeFirst', true);
+
+      this.render(hbs`{{shhh (compute (if takeFirst firstStep secondStep) 'lets do it')}}`);
+
+      expect(called).to.equal(1);
+      expect(result).to.equal('first step: lets do it');
+      expect(this.$().text()).to.equal('');
+
+      this.set('takeFirst', false);
+
+      expect(called).to.equal(2);
+      expect(result).to.equal('second step: lets do it');
+      expect(this.$().text()).to.equal('');
+      
     });
   }
 );
